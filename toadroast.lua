586600c01f81b18508711f6da27db5a507fc6860
unfunnylaugh <129692077+unfunnylaugh@users.noreply.github.com>
2023-04-23 13:58:47 -0300

    Create toadroast.lua

---
+++ toadroast.lua
@@ -0,0 +1,82 @@
+wait(10)
+epic=Instance.new("Hint")
+epic.Text = "HAXED BY TEAM H1 OMAYHGOT!111!!"
+epic.Parent = game.Workspace
+--heheheheheeh epicccccccc
+wait(5)
+epic=Instance.new("Message")
+epic.Text = "AMBOTAKUM H1 IS HERE"
+epic.Parent = game.Workspace
+wait(3)
+epic:remove()
+hehe=Instance.new("Sky")
+hehe.Parent = game.Lighting
+hehe.SkyboxBk = "http://www.roblox.com/asset/?id=3411957784"
+hehe.SkyboxDn = "http://www.roblox.com/asset/?id=3411957784"
+hehe.SkyboxFt = "http://www.roblox.com/asset/?id=3411957784"
+hehe.SkyboxLf = "http://www.roblox.com/asset/?id=3411957784"
+hehe.SkyboxRt = "http://www.roblox.com/asset/?id=3411957784"
+hehe.SkyboxUp = "http://www.roblox.com/asset/?id=3411957784"
+hehe.CelestialBodiesShown = false
+wait(1)
+
+while true do
+	pilla=math.random(0,3)
+	crex=math.random(3,100)
+	aiko=math.random(-500, 500)
+	jeepeep=math.random(100, 600)
+	gimmecat=math.random(-500, 500)
+	wait()
+--epiccccccccccccccccccccccccc
+	doom=Instance.new("Part")
+	doom.Shape = "Block"
+	doom.Size = Vector3.new(crex, crex, crex)
+	doom.Position = Vector3.new(aiko, jeepeep, gimmecat)
+	doom.Color = Color3.new(1, 0, 0)
+	doom.Velocity=Vector3.new(pilla, pilla, pilla)
+	doom.Parent = game.Workspace
+	ham5ter=math.random(-200, -50)
+	doom=Instance.new("Part")
+	doom.Shape = "Block"
+	doom.Size = Vector3.new(crex, crex, crex)
+	doom.Position = Vector3.new(aiko, ham5ter, gimmecat)
+	doom.Color = Color3.new(1, 0, 0)
+	doom.Velocity=Vector3.new(pilla, 800, pilla)
+	doom.Parent = game.Workspace
+		pilla=math.random(0,3)
+	crex=math.random(3,100)
+	aiko=math.random(-500, 500)
+	jeepeep=math.random(100, 600)
+	gimmecat=math.random(-500, 500)
+	doom=Instance.new("Part")
+	doom.Shape = "Block"
+	doom.Size = Vector3.new(crex, crex, crex)
+	doom.Position = Vector3.new(aiko, jeepeep, gimmecat)
+	doom.Color = Color3.new(1, 0, 0)
+	doom.Velocity=Vector3.new(pilla, pilla, pilla)
+	doom.Parent = game.Workspace
+		pilla=math.random(0,3)
+	crex=math.random(3,100)
+	aiko=math.random(-500, 500)
+	jeepeep=math.random(100, 600)
+	gimmecat=math.random(-500, 500)
+	doom=Instance.new("Part")
+	doom.Shape = "Block"
+	doom.Size = Vector3.new(crex, crex, crex)
+	doom.Position = Vector3.new(aiko, jeepeep, gimmecat)
+	doom.Color = Color3.new(1, 0, 0)
+	doom.Velocity=Vector3.new(pilla, pilla, pilla)
+	doom.Parent = game.Workspace
+		pilla=math.random(0,3)
+	crex=math.random(3,100)
+	aiko=math.random(-500, 500)
+	jeepeep=math.random(100, 600)
+	gimmecat=math.random(-500, 500)
+	doom=Instance.new("Part")
+	doom.Shape = "Block"
+	doom.Size = Vector3.new(crex, crex, crex)
+	doom.Position = Vector3.new(aiko, jeepeep, gimmecat)
+	doom.Color = Color3.new(1, 0, 0)
+	doom.Velocity=Vector3.new(pilla, pilla, pilla)
+	doom.Parent = game.Workspace
+end
